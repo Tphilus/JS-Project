a1a7dba651a0b3b17df7841b6ff680a0810b5b66
TheophilusDadzie <tkdadzie@st.ug.edu.gh>
2022-08-19 02:49:31 +0000

    add commit

---
+++ EMOJI-RATING/Script.js
@@ -0,0 +1,26 @@
+const starsEl = document.querySelectorAll(".fa-star");
+const emojiEl = document.querySelectorAll(".far");
+const colorsArr = ["red", "orange", "lightblue", "lightgreen", "green"];
+
+
+updateRating(0);
+starsEl.forEach((starsEl, index) => {
+    starsEl.addEventListener("click", () => {
+        updateRating(index);
+    })
+})
+
+function updateRating(index) {
+    starsEl.forEach((starsEl, idx) => {
+        if(idx < index + 1) {
+            starsEl.classList.add("active");
+        } else {
+            starsEl.classList.remove("active");
+        }
+    })
+    
+    emojiEl.forEach((emojiEl) => {
+        emojiEl.style.transform = `translateX(-${index * 50}px)`;
+        emojiEl.style.color = colorsArr[index];
+    })
+}
