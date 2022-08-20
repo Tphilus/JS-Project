4a4fa506beaaef69770d10b59a87930394760502
TheophilusDadzie <tkdadzie@st.ug.edu.gh>
2022-08-20 01:00:21 +0000

    second commit

--- PROFILE STATISCS/script.js
+++ PROFILE STATISCS/script.js
@@ -2,4 +2,19 @@ const countersEl = document.querySelectorAll(".counter");
 
 countersEl.forEach((countersEl) => {
     countersEl.innerText = "0";
+    incrementCounter()
+    function incrementCounter() {
+       let currentNum = +countersEl.innerText;
+       const dataCeil = countersEl.getAttribute("data-ceil");
+       const increment = dataCeil / 15;
+       currentNum += Math.ceil(increment);
+       
+
+       if(currentNum <  dataCeil) {
+        countersEl.innerText = currentNum;
+        setTimeout (incrementCounter, 50);
+       } else {
+        countersEl.innerText = currentNum;
+       }
+    }
 })
